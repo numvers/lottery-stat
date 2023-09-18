afdd985c21fd25527d62e6a015c86f9f2a188a81
Yduuduu <hou_o@naver.com>
2023-09-18 23:00:32 +0900

    Chore: next.config.mjs styledComponents 설정 추가

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,9 +16,11 @@ export default function Layout({ children }: LayoutProps) {
 const Centering = styled.div`
   display: flex;
   justify-content: center;
+  background-color: gainsboro;
 `;
 const FixedWidth = styled.div`
   width: 500px;
+  background-color: wheat;
   @media (max-width: 500px) {
     /* 화면 너비가 500px 이하가 되면 요소 너비를 100%로 고정*/
     width: 100%;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Seo title="numbers" />
-      <main className="bg-num-yellow flex h-screen flex-col items-center justify-center">
+      <main className=" flex h-screen flex-col items-center justify-center">
         <div className="sm:bg-num-blue md:bg-num-red bg-num-green container flex    flex-col items-center justify-center">
           테스트
         </div>
