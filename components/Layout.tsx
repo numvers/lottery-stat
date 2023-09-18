9509cc3e5683252ca307b33b9cb0303a92bc4284
Yduuduu <hou_o@naver.com>
2023-09-18 18:14:46 +0900

    Design: Layout 코드 수정

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import NavBar from "./NavBar";
+import styled from "styled-components";
 import { ReactNode } from "react";
 
 interface LayoutProps {
@@ -7,9 +7,20 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <>
-      {/* <NavBar /> */}
-      <div>{children}</div>
-    </>
+    <Centering>
+      <FixedWidth>{children}</FixedWidth>
+    </Centering>
   );
 }
+
+const Centering = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const FixedWidth = styled.div`
+  width: 500px;
+  @media (max-width: 500px) {
+    /* 화면 너비가 500px 이하가 되면 요소 너비를 100%로 고정*/
+    width: 100%;
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <Seo title="numbers" />
       <main className="bg-num-yellow flex h-screen flex-col items-center justify-center">
-        <div className="sm:bg-num-blue md:bg-num-red bg-num-green container flex h-screen  w-[500px]  flex-col items-center justify-center sm:w-screen">
+        <div className="sm:bg-num-blue md:bg-num-red bg-num-green container flex    flex-col items-center justify-center">
           테스트
         </div>
       </main>
