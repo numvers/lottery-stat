a0ccd0bfadc4adaddad17eb71b6563259b0ec029
raeperd <raeperd117@gmail.com>
2023-09-24 11:14:02 +0900

    feat: /select/list page 구현

--- src/components/UserLottery.tsx
+++ src/components/UserLottery.tsx
@@ -11,7 +11,7 @@ export default function UserLottery(props: UserLotteryProps) {
       <div className="flex items-center justify-between rounded-[1.25rem] bg-gray_4 px-[1.25rem] py-[0.87rem]">
         <div>
           <h2 className="flex items-center gap-1 font-semibold">
-            {props.nickname}
+            {ShortenNickname(props.nickname)}
             {props.menu == "uju" && (
               <Image
                 src="/img/icon_spaceship.svg"
@@ -22,7 +22,9 @@ export default function UserLottery(props: UserLotteryProps) {
               />
             )}
           </h2>
-          <span className="text-xs">23.09.12</span>
+          <span className="text-center text-xs">
+            {props.created_at.slice(0, 10)}
+          </span>
         </div>
         <LotteryNumberBallSmall numbers={props.numbers} bonus={true} />
       </div>
@@ -36,4 +38,11 @@ interface UserLotteryProps {
   menu: string;
   created_at: string;
   nickname: string;
+}
+
+function ShortenNickname(name: string) {
+  if (7 < name.length) {
+    return `${name.slice(0, 7)} ..`;
+  }
+  return name;
 }
